bbed730b2d29dc2045737c5fffde04b773d69322
hxkim10 <abdelhakimtiabi@gmail.com>
2023-10-01 14:41:09 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  
+
+  const seenNumbers = {};
+  
+  for (const number of array) {
+   
+    const complement = target - number;
+    
+    if (seenNumbers[complement]) return true;
+        seenNumbers[number] = true;
+  }
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +26,23 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
